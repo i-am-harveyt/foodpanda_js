bdca9fbe8ed681d141176cbbe0f076e337cc6e14
i-am-harveyt <haotingtong30@gmail.com>
2024-05-09 12:54:37 +0800

    Add: column

--- src/getMenu/extractMenu.js
+++ src/getMenu/extractMenu.js
@@ -5,35 +5,40 @@
  * @return {object} - The generated menu object.
  */
 export default function getMenuData(data) {
-  let result = {
-    id: [],
-    code: [],
-    product: [],
-    description: [],
-    variations: {
-      code: [],
-      name: [],
-      preDiscountPrice: [],
-      discountedPrice: [],
-    },
-    isSoldOut: [],
-  };
-  for (const category of data.menus[0].menu_categories) {
-    for (const item of category.products) {
-      result.id.push(item.id ? item.id : NaN);
-      result.code.push(item.code ? item.code : NaN);
-      result.product.push(item.name ? item.name : NaN);
-      result.description.push(item.description ? item.description : NaN);
-      result.isSoldOut.push(item.is_sold_out);
+	let result = {
+		id: [],
+		code: [],
+		product: [],
+		description: [],
+		variations: {
+			code: [],
+			name: [],
+			preDiscountPrice: [],
+			discountedPrice: [],
+		},
+		isSoldOut: [],
+		tags: [],
+		description: [],
+	};
+	for (const category of data.menus[0].menu_categories) {
+		for (const item of category.products) {
+			result.id.push(item.id ? item.id : NaN);
+			result.code.push(item.code ? item.code : NaN);
+			result.product.push(item.name ? item.name : NaN);
+			result.description.push(item.description ? item.description : NaN);
+			result.isSoldOut.push(item.is_sold_out);
 
-      // variations
-      for (const variation of item.product_variations) {
-        result.variations.code.push(variation.code);
-        result.variations.name.push(variation.name);
-        result.variations.preDiscountPrice.push(variation.price_before_discount);
-        result.variations.discountedPrice.push(variation.price);
-      }
-    }
-  }
-  return result;
+			// variations
+			for (const variation of item.product_variations) {
+				result.variations.code.push(variation.code);
+				result.variations.name.push(variation.name);
+				result.variations.preDiscountPrice.push(
+					variation.price_before_discount
+				);
+				result.variations.discountedPrice.push(variation.price);
+			}
+      result.tags.push(item.tags)
+		}
+	}
+	return result;
 }

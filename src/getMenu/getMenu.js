a5e8dc9956f6fe94ce98f4e8d87546f893f54cac
i-am-harveyt <haotingtong30@gmail.com>
2024-11-08 13:46:23 +0800

    Modify: determine date to grep json

--- src/getMenu/getMenu.js
+++ src/getMenu/getMenu.js
@@ -13,43 +13,49 @@ import { exit } from "process";
  * @param {number} longitude
  */
 export default async function getMenu(
-	cookie,
-	shopUuid,
-	shopName,
-	latitude,
-	longitude
+  cookie,
+  shopUuid,
+  shopName,
+  latitude,
+  longitude,
+  grepJson,
 ) {
-	// delay
-	await new Promise((resolve) =>
-		setTimeout(resolve, 1_000 * 5 + Math.random() * 1_000)
-	);
+  // delay
+  await new Promise((resolve) =>
+    setTimeout(resolve, 1_000 * 5 + Math.random() * 1_000),
+  );
 
-	if (Object.keys(cookie.cookies).length === 0) {
-		let get = await fetch(
-			`https://www.foodpanda.com.tw/restaurant/${shopUuid}/`
-		);
-		console.log(shopUuid, latitude, longitude, get.status);
-		cookie.updateCookies(get.headers.getSetCookie().join("; "));
-	}
+  if (Object.keys(cookie.cookies).length === 0) {
+    let get = await fetch(
+      `https://www.foodpanda.com.tw/restaurant/${shopUuid}/`,
+    );
+    console.log(shopUuid, latitude, longitude, get.status);
+    cookie.updateCookies(get.headers.getSetCookie().join("; "));
+  }
 
-	let now = new Date();
+  let now = new Date();
 
-	// fetch logic
-	let response = await sendReqMenu(cookie, shopUuid, latitude, longitude);
-	console.log(shopUuid, latitude, longitude, response.status);
-	cookie.updateCookies(response.headers.getSetCookie().join("; "));
-	const data = await response.json();
+  // fetch logic
+  let response = await sendReqMenu(cookie, shopUuid, latitude, longitude);
+  console.log(shopUuid, latitude, longitude, response.status);
+  cookie.updateCookies(response.headers.getSetCookie().join("; "));
+  const data = await response.json();
 
-	// write to json
-	// try {
-	// 	const TODAY = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-	// 	const jsonPath = `../../../panda_data_js/panda_menu/json/${TODAY}`;
-	// 	mkdirSync(jsonPath, { recursive: true });
-	// 	writeFileSync(`${jsonPath}/${latitude}_{longitude}_${shopUuid}.json`, JSON.stringify(data));
-	// } catch (error) {
-	// 	console.log(error);
-	// }
+  // write to json
+  if (grepJson) {
+    try {
+      const TODAY = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+      const jsonPath = `../../../panda_data_js/panda_menu/json/${TODAY}`;
+      mkdirSync(jsonPath, { recursive: true });
+      writeFileSync(
+        `${jsonPath}/${latitude}_{longitude}_${shopUuid}.json`,
+        JSON.stringify(data),
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	// data conversion
-	return extractData(data.data, now, latitude, longitude);
+  // data conversion
+  return extractData(data.data, now, latitude, longitude);
 }
--- src/getMenu/main.js
+++ src/getMenu/main.js
@@ -7,78 +7,80 @@ import { DataFrame } from "danfojs-node";
 const DEBUG_MODE = false;
 
 async function main() {
-	const date = new Date();
+  const date = new Date();
 
-	let TODAY = `${date.getFullYear()}`;
-	if (date.getMonth() + 1 < 10) TODAY += `-0${date.getMonth() + 1}`;
-	else TODAY += `-${date.getMonth() + 1}`;
-	if (date.getDate() < 10) TODAY += `-0${date.getDate()}`;
-	else TODAY += `-${date.getDate()}`;
+  let TODAY = `${date.getFullYear()}`;
+  if (date.getMonth() + 1 < 10) TODAY += `-0${date.getMonth() + 1}`;
+  else TODAY += `-${date.getMonth() + 1}`;
+  if (date.getDate() < 10) TODAY += `-0${date.getDate()}`;
+  else TODAY += `-${date.getDate()}`;
 
-	const PATH = `../../../panda_data_js/panda_menu/${TODAY}`;
+  const PATH = `../../../panda_data_js/panda_menu/${TODAY}`;
 
-	// 確保輸出目錄存在
-	mkdirSync(PATH, { recursive: true });
+  // 確保輸出目錄存在
+  mkdirSync(PATH, { recursive: true });
 
-	// init cookie
-	let cookie = new Cookie();
-	cookie.init();
+  // init cookie
+  let cookie = new Cookie();
+  cookie.init();
 
-	// 如果在 debug 模式，側特定的店家就夠了
-	if (DEBUG_MODE) {
-		await getMenu(
-			cookie,
-			"abmy",
-			"",
-			25.0531908,
-			121.45147382,
-		)
-		return;
-	}
+  // 如果在 debug 模式，側特定的店家就夠了
+  if (DEBUG_MODE) {
+    await getMenu(cookie, "abmy", "", 25.0531908, 121.45147382, true);
+    return;
+  }
 
-	// read shopinformation
-	const locationPath = `../../../panda_data/shopLst/${TODAY}`;
-	let locationLst = readdirSync(locationPath);
-	const menuPath = `../../../panda_data_js/panda_menu/${TODAY}`;
+  // read shopinformation
+  const locationPath = `../../../panda_data/shopLst/${TODAY}`;
+  let locationLst = readdirSync(locationPath);
+  const menuPath = `../../../panda_data_js/panda_menu/${TODAY}`;
 
+  for (const location of locationLst) {
+    if (!location.startsWith("shopLst")) continue;
+    let stores = [];
+    let df = await readCSV(`${locationPath}/${location}`);
+    try {
+      df = df.loc({
+        columns: [
+          "shopCode",
+          "shopName",
+          "anchor_latitude",
+          "anchor_longitude",
+        ],
+      }).values;
+    } catch (e) {
+      console.error(e);
+      continue;
+    }
+    console.log(`(${df[0][2]}, ${df[0][3]}): ${df.length} shops`);
+    for (const row of df)
+      try {
+        stores.push(
+          await getMenu(
+            cookie,
+            row[0],
+            row[1],
+            row[2],
+            row[3],
+            date.getDate() >= 10 && date.getDate() < 17,
+          ),
+        );
+      } catch (e) {
+        console.error(e);
+      }
+    const result = new DataFrame(stores);
+    result.toCSV({
+      filePath: `${menuPath}/${location}_${TODAY}.csv`,
+      header: true,
+    });
+  }
 
-	for (const location of locationLst) {
-		if (!location.startsWith("shopLst")) continue;
-		let stores = [];
-		let df = await readCSV(`${locationPath}/${location}`);
-		try {
-			df = df.loc({
-				columns: [
-					"shopCode",
-					"shopName",
-					"anchor_latitude",
-					"anchor_longitude",
-				],
-			}).values;
-		} catch (e) {
-			console.error(e);
-			continue;
-		}
-		console.log(`(${df[0][2]}, ${df[0][3]}): ${df.length} shops`);
-		for (const row of df)
-			try {
-				stores.push(await getMenu(cookie, row[0], row[1], row[2], row[3]));
-			} catch (e) {
-				console.error(e);
-			}
-		const result = new DataFrame(stores);
-		result.toCSV({
-			filePath: `${menuPath}/${location}_${TODAY}.csv`,
-			header: true,
-		});
-	}
-
-	console.log("down shop catch");
+  console.log("down shop catch");
 }
 
 try {
-	main();
+  main();
 } catch (e) {
-	console.info("Totally failed");
-	console.error(e);
+  console.info("Totally failed");
+  console.error(e);
 }
